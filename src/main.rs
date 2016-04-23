dbaa08d5b36431aa8ebb6b773b93a805836c7e53
eeeeeta <eeeeeta@users.noreply.github.com>
2016-04-23 19:25:42 +0100

    Command-based control
    I'm trying all sorts of ways to control this thing. Bear with me.

--- src/main.rs
+++ src/main.rs
@@ -1,30 +1,33 @@
 extern crate rsndfile;
 extern crate portaudio;
+#[macro_use]
+extern crate nom;
 extern crate time;
 extern crate uuid;
 extern crate crossbeam;
 
 mod streamv2;
 mod mixer;
-mod cues;
+mod cmdi;
 
 use std::thread;
+use std::io;
+use streamv2::db_lin;
+use rsndfile::SndFile;
+use std::io::BufRead;
+use std::collections::BTreeMap;
 use uuid::Uuid;
 use time::Duration;
 use std::rc::Rc;
 use std::cell::RefCell;
 use mixer::{Source, Sink, FRAMES_PER_CALLBACK};
-use cues::{Q, AudioQ, FadeQ, QParam, WireableInfo, QList};
+use streamv2::{FileStream, FileStreamX};
 use portaudio as pa;
-fn inspect_q_params(q: &Q) {
-    for (strn, uuid, qp) in q.get_params() {
-        println!("[*] \"{}\" (UUID {}): {:?}", strn, uuid, qp);
-    }
-}
+use cmdi::Command;
 fn main() {
     let mut pa = pa::PortAudio::new().unwrap();
     let mut mstr = mixer::Magister::new();
-
+    let mut streams: BTreeMap<String, Vec<FileStreamX>> = BTreeMap::new();
     println!("[+] Initialising output device...");
 
     let def_out = pa.default_output_device().unwrap();
@@ -67,69 +70,64 @@ fn main() {
             _ => {}
         }
     }
-    let wrapped_mstr = Rc::new(RefCell::new(mstr));
-    println!("[+] Creating some cool Audio Cues...");
-    let mut aq1 = AudioQ::new(wrapped_mstr.clone());
-    let uu1 = aq1.uuid();
-    println!("[+] AQ1 parameter listing:");
-    inspect_q_params(&aq1);
-    let mut aq2 = AudioQ::new(wrapped_mstr.clone());
-    let uu2 = aq2.uuid();
-    println!("[+] AQ2 parameter listing:");
-    inspect_q_params(&aq1);
-    println!("[+] Setting up QList...");
-    let mut ql = QList::new();
-    println!("[+] Setting file paths...");
-    let fpu1 = aq1.get_params()[0].1;
-    let fpu2 = aq2.get_params()[0].1;
-    println!("[*] Setting AQ1's path as './test.aiff': {:?}", aq1.set_param(fpu1, QParam::FilePath(Some(format!("./test.aiff")))));
-    println!("[*] Setting AQ2's path as './meows.aiff': {:?}", aq2.set_param(fpu2, QParam::FilePath(Some(format!("./meows.aiff")))));
-    println!("[+] AQ1 parameter listing:");
-    inspect_q_params(&aq1);
-    println!("[+] AQ2 parameter listing:");
-    inspect_q_params(&aq2);
-
-    println!("[+] Wiring Audio Cues...");
-    println!("[*] Getting Wireables of AQ1...");
-    for WireableInfo(is_source, cid, uuid) in aq1.wireables() {
-        println!("[*] Wireable: source {}, cid {}, uuid {}", is_source, cid, uuid);
-        println!("[*] Wiring to Q1... {:?}", wrapped_mstr.borrow_mut().wire(uuid, c1_u));
-    }
-    println!("[*] Getting Wireables of AQ2...");
-    for WireableInfo(is_source, cid, uuid) in aq2.wireables() {
-        println!("[*] Wireable: source {}, cid {}, uuid {}", is_source, cid, uuid);
-        println!("[*] Wiring to Q2... {:?}", wrapped_mstr.borrow_mut().wire(uuid, c2_u));
-    }
-    ql.insert(Box::new(aq1));
-    ql.insert(Box::new(aq2));
-
-    println!("\n[+] Creating Fade Cue...");
-    let mut fq1 = FadeQ::new();
-    println!("[*] FQ params:");
-    inspect_q_params(&fq1);
-    let fpu25 = fq1.get_params()[3].1;
-    println!("[+] Setting fade time to 10s... {:?}", fq1.set_param(fpu25, QParam::Duration(Duration::milliseconds(10000))));
-    println!("[*] FQ warnings: {:?}", fq1.warnings(&ql));
-    println!("[+] Targeting chans of AQ1...");
-    let fpu3 = fq1.get_params()[1].1;
-    let fpu4 = fq1.get_params()[0].1;
-    println!("[*] Targeting AQ1... {:?}", fq1.set_param(fpu3, QParam::UuidTarget(Some(uu1))));
-    println!("[*] FQ warnings: {:?}", fq1.warnings(&ql));
-    for ch in ql.cues.get(&uu1).unwrap().get_params() {
-        if let QParam::Volume(chan, _) = ch.2 {
-            println!("[*] Targeting channel {}... {:?}", chan, fq1.set_param(fpu4, QParam::VecInsert(0, Box::new(QParam::UuidTarget(Some(ch.1))))));
+    println!("\n[+] Right! Type away.");
+    let stdin = io::stdin();
+    for line in stdin.lock().lines() {
+        let ln = line.unwrap();
+        let res = cmdi::command(&ln);
+        if let ::nom::IResult::Done(left, cmd) = res {
+            if left.len() > 0 {
+                println!("[*] warning: unparsed section '{}'", left);
+            }
+            match cmd {
+                Command::Load(filen, optident) => {
+                    let file = SndFile::open(filen);
+                    if let Err(snde) = file {
+                        println!("[-] couldn't open file: {}", snde.expl);
+                        continue;
+                    }
+                    if file.as_ref().unwrap().info.samplerate != 44_100 {
+                        println!("[-] sample rate mismatch");
+                        continue;
+                    }
+                    let fs = FileStream::new(file.unwrap());
+                    let mut cvec = vec![];
+                    for stream in fs.into_iter() {
+                        cvec.push(stream.get_x());
+                        mstr.add_source(Box::new(stream));
+                    }
+                    for ch in cvec.iter() {
+                        mstr.wire(ch.uuid(), c1_u).unwrap();
+                    }
+                    let mut ident: &str = ::std::path::Path::new(filen).file_stem().unwrap().to_str().unwrap();
+                    if optident.is_some() {
+                        ident = optident.unwrap();
+                    }
+                    streams.insert(ident.to_owned(), cvec);
+                    println!("[+] Loaded '{}' using identifier '{}'", filen, ident);
+                },
+                Command::Vol(ident, chan, vol, fade) => {
+                    if let Some(fsx) = streams.get_mut(ident) {
+                        if chan >= fsx.len() as i32 {
+                            println!("[-] invalid channel");
+                            continue;
+                        }
+                        for (i, ch) in fsx.iter_mut().enumerate() {
+                            if chan == -1 || chan as usize == i {
+                                println!("[*] Setting ${}:{} to {}dB ({})", ident, i, vol, db_lin(vol));
+                                ch.set_vol(db_lin(vol));
+                            }
+                        }
+                    }
+                    else {
+                        println!("[-] unknown identifier");
+                    }
+                },
+                _ => unimplemented!()
+            }
         }
-    }
-    println!("[*] FQ warnings: {:?}", fq1.warnings(&ql));
-    println!("\n[+] Coolio. Hitting go on all cues...");
-    ql.cues.get_mut(&uu1).unwrap().go();
-    ql.cues.get_mut(&uu2).unwrap().go();
-    fq1.go();
-        thread::sleep(::std::time::Duration::from_millis(3000));
-    println!("[+] Starting FQ loop...");
-    loop {
-        println!("polling...");
-        thread::sleep(::std::time::Duration::from_millis(100));
-        fq1.poll(&mut ql, Duration::milliseconds(100));
+        else {
+            println!("[-] parse failed: {:?}", res);
+        }
     }
 }
